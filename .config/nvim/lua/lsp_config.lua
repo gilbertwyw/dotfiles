860ed7555e6006f11e120a02c90b6f3d914fa933
GW <jaguar.fender@gmail.com>
2021-01-09 19:22:50 +0800

    [vim] tweak LSP client configuration
    Group some mappings under localleader at the moment.
    
    References:
    - https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
    - https://github.com/nvim-lua/completion-nvim#setup

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -1,32 +1,50 @@
-vim.lsp.set_log_level(1) -- debug
+vim.lsp.set_log_level("debug")
 
-local custom_lsp_attach = function(client)
+local nvim_lsp = require('lspconfig')
+local on_attach = function(client, bufnr)
   -- See `:help nvim_buf_set_keymap()` for more information
-  vim.api.nvim_buf_set_keymap(0, 'n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>gd', '<cmd>lua vim.lsp.buf.declaration()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', {noremap = true})
-  vim.api.nvim_buf_set_keymap(0, 'n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', {noremap = true})
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Use LSP as the handler for omnifunc.
   --    See `:help omnifunc` and `:help ins-completion` for more information.
-  vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- For plugins with an `on_attach` callback, call them here. For example:
-  require('completion').on_attach(client)
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<LocalLeader>gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<LocalLeader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<LocalLeader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<LocalLeader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<LocalLeader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gK', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "<LocalLeader>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("n", "<LocalLeader>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  end
+
+  require('completion').on_attach()
+end
+
+local servers = { "dartls", "tsserver" }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup { on_attach = on_attach }
 end
 
 -- https://github.com/neovim/nvim-lspconfig#gopls
 -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#custom-configuration
-local lspconfig = require'lspconfig'
-lspconfig.gopls.setup {
-  on_attach = custom_lsp_attach,
+nvim_lsp.gopls.setup {
+  on_attach = on_attach,
   settings = {
     -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
     gopls = {
@@ -40,11 +58,4 @@ lspconfig.gopls.setup {
   },
 }
 
-lspconfig.dartls.setup{
-  on_attach = custom_lsp_attach,
-}
-
-lspconfig.tsserver.setup{
-  on_attach = custom_lsp_attach,
-}
 
