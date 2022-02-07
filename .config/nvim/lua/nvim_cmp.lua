98329d0634f4560a2f9e85c23289ab9996661a2e
Gilbert Wong <wonggilbert@pm.me>
2022-02-07 22:42:26 +0800

    refactor(vim): reorder the sources for nvim-cmp

--- .config/nvim/lua/nvim_cmp.lua
+++ .config/nvim/lua/nvim_cmp.lua
@@ -1,23 +1,25 @@
 -- https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/default.lua
 local cmp = require'cmp'
+
 cmp.setup({
   sources = {
-    { name = 'buffer' }, 
+    -- order matters
     { name = 'nvim_lsp' },
-    { name = 'path' },
     { name = 'ultisnips' },
     { name = 'vsnip' },
+    { name = 'buffer' },
+    { name = 'path' },
   }
 })
 
--- Use buffer source for `/`.
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
   sources = {
     { name = 'buffer' }
   }
 })
 
--- Use cmdline & path source for ':'.
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
   sources = cmp.config.sources({
     { name = 'path' }
