bf315d5136e3b796f9950a2070520296b5919392
Gilbert Wong <wonggilbert@pm.me>
2022-07-18 14:36:26 +0800

    chore(neovim): reduce the step to run vim-slime
    See: https://blog.jpalardy.com/posts/trying-vim-slime-in-docker/

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -505,6 +505,11 @@ return require('packer').startup({
       'jpalardy/vim-slime',
       config = function()
         vim.g.slime_target = 'tmux'
+        vim.g.slime_default_config = {
+          socket_name = "default",
+          target_pane = "{last}",
+        }
+        vim.g.slime_dont_ask_default = 1
       end
     }
     use {
