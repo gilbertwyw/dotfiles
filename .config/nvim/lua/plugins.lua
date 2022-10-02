7fb7943dddaa6fd6e498193b053b94fc8e3dab06
Gilbert Wong <wonggilbert@pm.me>
2022-10-02 21:04:43 +1300

    refactor(neovm): remove unnecessary plugin for Nvim v0.8.0
    See:
    
    - https://github.com/neovim/neovim/pull/20198
    - https://github.com/neovim/neovim/releases/tag/v0.8.0

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -22,13 +22,6 @@ return require('packer').startup({
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    -- fix
-    use { 'antoinemadec/FixCursorHold.nvim', config = function()
-      -- in millisecond, used for both CursorHold and CursorHoldI,
-      -- use updatetime instead if not defined
-      vim.g.cursorhold_updatetime = 100
-    end
-    }
 
     -- telescope
     use {
