e308232c2fd47e5f3de1cffa387f45efb71ad9d0
Gilbert Wong <wonggilbert@pm.me>
2022-12-30 15:48:22 +0800

    refactor(neovim): mappings for Telescope
    Use `desc` in options to display them in `builtin.keymaps`.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,34 +33,47 @@ require('packer').startup({
       } },
       config = function()
         local telescope = require('telescope')
+        -- extensions
         telescope.load_extension('file_browser')
         telescope.load_extension('fzf')
         telescope.load_extension('project')
 
-        vim.keymap.set('n', '<C-p>', ":lua require'telescope'.extensions.project.project{display_type = 'full'}<cr>")
-        vim.keymap.set('n', '<localleader>,', ':Telescope builtin<cr>')
-        vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<cr>')
-        vim.keymap.set('n', '<leader>ff', ':Telescope file_browser path=%:p:h <cr>')
-        vim.keymap.set('n', '<leader>.', ':Telescope resume<cr>')
-        vim.keymap.set('n', '<leader>/', ':Telescope search_history<cr>')
-        vim.keymap.set('n', '<leader><space>',
-          ':Telescope find_files find_command=fd,--color,never,--type,f,--hidden,--follow,--exclude,.git<cr>')
-        vim.keymap.set('n', '<leader>bb', ':Telescope buffers<cr>')
-        vim.keymap.set('n', '<leader>bc', ':Telescope git_bcommits<cr>')
-        vim.keymap.set('n', '<leader>bl', ':Telescope current_buffer_fuzzy_find<cr>')
-        vim.keymap.set('n', '<leader>bt', ':Telescope current_buffer_tags<cr>')
-        vim.keymap.set('n', '<leader>cl', ':Telescope colorscheme<cr>')
-        vim.keymap.set('n', '<leader>cm', ':Telescope commands<cr>')
-        vim.keymap.set('n', '<leader>fr', ':Telescope oldfiles<cr>')
-        vim.keymap.set('n', '<leader>ft', ':Telescope filetypes<cr>')
-        vim.keymap.set('n', '<leader>gf', ':Telescope git_files<cr>')
-        vim.keymap.set('n', '<leader>gs', ':Telescope git_status<cr>')
-        vim.keymap.set('n', '<leader>km', ':Telescope keymaps<cr>')
-        vim.keymap.set('n', '<leader>m', ':Telescope marks<cr>')
-        vim.keymap.set('n', '<leader>sp', ':Telescope live_grep<cr>')
-        vim.keymap.set('n', '<localleader>ht', ':Telescope help_tags<cr>')
-        vim.keymap.set('n', '<localleader>tt', ':Telescope tags<cr>')
-        vim.keymap.set('n', 'gs', ':Telescope grep_string<cr>')
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<C-p>', function() telescope.extensions.project.project { display_type = 'full' } end,
+          { desc = 'Projects' })
+        vim.keymap.set('n', '<localleader>,', builtin.builtin, { desc = 'Built-in pickers' })
+        vim.keymap.set('n', '<leader>.', builtin.resume, { desc = 'Previous picker' })
+        vim.keymap.set('n', '<leader>/', builtin.search_history, { desc = 'Search history' })
+
+        vim.keymap.set('n', '<leader><space>', builtin.find_files, { desc = 'Find files' })
+        -- --unrestricted: an alias for '--hidden --no-ignore'
+        vim.keymap.set('n', '<leader>F',
+          function() builtin.find_files({ find_command = { 'fd', '--unrestricted', '--color', 'never', '--type', 'f' } }) end
+          , { desc = 'Files (All)' })
+        vim.keymap.set('n', '<leader>f.',
+          function() telescope.extensions.file_browser.file_browser { path = '%:p:h' } end,
+          { desc = 'Files in current directory' })
+        vim.keymap.set('n', '<leader>fb', telescope.extensions.file_browser.file_browser, { desc = 'File browser' })
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Recently opened files' })
+        vim.keymap.set('n', '<leader>ft', builtin.filetypes, { desc = 'Filetypes' })
+
+        vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'Buffers' })
+        vim.keymap.set('n', '<leader>bc', builtin.git_bcommits, { desc = 'Commits for current buffer' })
+        vim.keymap.set('n', '<leader>bl', builtin.current_buffer_fuzzy_find,
+          { desc = 'Live fuzzy search inside of the currently open buffer' })
+        vim.keymap.set('n', '<leader>bt', builtin.current_buffer_tags, { desc = 'Tags for the currently open buffer' })
+
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Files tracked by Git' })
+        vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Git status' })
+
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[D]iagnostics' })
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[H]elp tags' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[K]ey mappings' })
+        vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[M]arks' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[G]rep' })
+        vim.keymap.set('n', '<leader>st', builtin.tags, { desc = '[T]ags' })
+        vim.keymap.set('n', 'gs', builtin.grep_string, { desc = 'Search current word' })
       end
     }
 
