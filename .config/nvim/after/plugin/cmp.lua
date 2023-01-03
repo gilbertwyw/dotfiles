1ff67ea2114fc48f53f75d543da31dd10f360979
Gilbert Wong <wonggilbert@pm.me>
2023-01-03 16:20:20 +0800

    refactor(neovim): move 'cmp' configuration

---
+++ .config/nvim/after/plugin/cmp.lua
@@ -0,0 +1,71 @@
+-- Defaults: https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/default.lua
+local cmp = require 'cmp'
+local lspkind = require 'lspkind'
+local luasnip = require 'luasnip'
+
+cmp.setup({
+  sources = cmp.config.sources({
+    -- order matters
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'luasnip' },
+    { name = 'nvim_lua' },
+    { name = 'path' },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true,
+    }),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  experimental = {
+    ghost_text = true,
+  },
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+require("luasnip.loaders.from_vscode").lazy_load()
