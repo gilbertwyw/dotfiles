fdb243e23b039052307aa29782f105afca992f2e
Gilbert Wong <gilbertwyw@gmail.com>
2023-03-22 00:26:17 +0800

    chore(neovim): update LSP config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,55 +1,23 @@
 -- vim.lsp.set_log_level("debug")
 
-vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
--- Add buffer diagnostics to the location list
-vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-local on_attach = function(client, bufnr)
-  -- See `:help omnifunc` and `:help ins-completion` for more information.
-  -- Enable completion triggered by <c-x><c-o>
-  -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-  vim.keymap.set('n', '<LocalLeader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<LocalLeader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-  vim.keymap.set('n', '<LocalLeader>gt', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<LocalLeader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<c-]>', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, bufopts)
-  vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, bufopts)
-
-  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<Leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-    bufopts)
-  vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<Leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-    { desc = '[W]orkspace [S]ymbols', noremap = true, silent = true, buffer = bufnr })
-
-  vim.keymap.set('n', 'g0', require('telescope.builtin').lsp_document_symbols, bufopts)
-end
-
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local servers = { "ansiblels", "bashls", "terraformls", "tsserver", "yamlls" }
+local servers = {
+  "ansiblels",
+  "bashls",
+  "terraformls",
+  "tsserver",
+  "yamlls",
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     capabilities = capabilities,
-    on_attach = on_attach,
   }
 end
 
 lspconfig.lua_ls.setup {
   capabilities = capabilities,
-  on_attach = on_attach,
   settings = {
     Lua = {
       diagnostics = {
@@ -63,7 +31,6 @@ lspconfig.lua_ls.setup {
 -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#custom-configuration
 lspconfig.gopls.setup {
   capabilities = capabilities,
-  on_attach = on_attach,
   settings = {
     -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
     gopls = {
@@ -76,3 +43,42 @@ lspconfig.gopls.setup {
     },
   },
 }
+
+vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+-- Add buffer diagnostics to the location list
+vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<LocalLeader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<LocalLeader>f', function() vim.lsp.buf.format { async = true } end, opts)
+    vim.keymap.set('n', '<LocalLeader>gt', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<LocalLeader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<c-]>', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+    vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, opts)
+
+    vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<Leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+      opts)
+    vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<Leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+      { desc = '[W]orkspace [S]ymbols', noremap = true, silent = true, buffer = ev.buf })
+
+    vim.keymap.set('n', 'g0', require('telescope.builtin').lsp_document_symbols, opts)
+  end
+})
