f598b650bd041c659e43fe8244bc3289e509391b
Gilbert Wong <gilbertwyw@gmail.com>
2023-08-29 00:01:46 +0800

    feat(neovim): add DAP support for python

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -72,6 +72,51 @@ require('lazy').setup({
     },
   },
 
+  -- DAP
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+
+      'mfussenegger/nvim-dap-python'
+    },
+    config = function()
+      local dap, dapui = require 'dap', require 'dapui'
+
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+
+      -- https://github.com/mfussenegger/nvim-dap-python#mappings
+      vim.keymap.set('n', '<Leader>dn', function() require('dap-python').test_method() end)
+      vim.keymap.set('n', '<Leader>df', function() require('dap-python').test_class() end)
+
+      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      vim.keymap.set('n', '<leader>dB', function()
+        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end, { desc = 'Debug: Set Breakpoint' })
+      vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+      vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+
+      dapui.setup()
+
+      -- https://github.com/rcarriga/nvim-dap-ui#usage
+      dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+      dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+      dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+      require("mason-nvim-dap").setup({
+        ensure_installed = { 'python' },
+        automatic_setup = true,
+        handlers = {}
+      })
+    end
+  },
+
   -- snippets
   'honza/vim-snippets',
 
