2fa295925331fa6fb13e070ce703c264c2a07f55
Gilbert Wong <gilbertwyw@gmail.com>
2024-03-27 19:43:14 +0800

    chore(neovm): update 'lazy.nvim' installation
    https://github.com/folke/lazy.nvim/commit/a836600573fbcf1879b850d40b8cdea59b33f8da

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,6 @@
 -- https://github.com/folke/lazy.nvim#-installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
