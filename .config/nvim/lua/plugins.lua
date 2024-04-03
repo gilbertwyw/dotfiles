999523b251a5ce5000f3856542ac239a779223ae
Gilbert Wong <wonggilbert@pm.me>
2024-04-03 18:10:06 +0800

    feat(neovim): automatically install language servers
    For those set up via lspconfig
    
    Details: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#default-configuration

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -56,6 +56,7 @@ require('lazy').setup({
           require("mason").setup()
           require("mason-lspconfig").setup {
             ensure_installed = { "lua_ls" },
+            automatic_installation = true,
           }
         end
       },
