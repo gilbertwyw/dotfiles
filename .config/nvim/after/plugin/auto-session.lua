d55d509cfc92207dddeef3f9c8686b23cc8b6723
Gilbert Wong <wonggilbert@pm.me>
2024-08-07 17:32:59 +0800

    refactor(Neovim): auto-session setup

---
+++ .config/nvim/after/plugin/auto-session.lua
@@ -0,0 +1,17 @@
+require("auto-session").setup {
+  auto_session_suppress_dirs = { "~/", "~/Downloads" },
+
+  -- ⚠️ This will only work if Telescope.nvim is installed
+  -- The following are already the default values, no need to provide them if these are already the settings you want.
+  session_lens = {
+    -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+    load_on_setup = true,
+    theme_conf = { border = true },
+    previewer = false,
+  },
+}
+
+-- couldn't use lazy's "key" because of lazy loading
+vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
+  noremap = true,
+})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -311,31 +311,9 @@ require('lazy').setup({
 
   {
     'rmagatti/auto-session',
-    config = function()
-      require("auto-session").setup {
-        log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Downloads" },
-
-        auto_session_use_git_branch = false,
-
-        auto_session_enable_last_session = false,
-
-        -- ⚠️ This will only work if Telescope.nvim is installed
-        -- The following are already the default values, no need to provide them if these are already the settings you want.
-        session_lens = {
-          -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-          buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-          load_on_setup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-      }
-
-      -- couldn't use lazy's "key" because of lazy loading
-      vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
-        noremap = true,
-      })
-    end,
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    }
   },
 
   {
