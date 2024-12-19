ed27b44cd557f1af82e050a126dcc9a163f793ec
Gilbert Wong <wonggilbert@pm.me>
2024-12-19 17:06:45 +0800

    chore(Neovim): use latest tag for telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,7 +16,7 @@ require('lazy').setup({
   -- telescope
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope-file-browser.nvim',
