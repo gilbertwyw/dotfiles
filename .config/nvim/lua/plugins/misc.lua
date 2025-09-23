e5d6c2ba0a03e47b5ca486ec876165955c4438d4
Gilbert Wong <gilbertwyw@gmail.com>
2025-09-23 23:35:24 +0800

    chore(Neovim): pin down the version for nvim-surround
    https://github.com/kylechui/nvim-surround?tab=readme-ov-file#lazynvim

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -11,7 +11,7 @@ return {
 
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     opts = {},
   },
