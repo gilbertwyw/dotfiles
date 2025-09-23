23d9ae970e987ff9dae47991f7ef5d0bbf48ee6c
Gilbert Wong <gilbertwyw@gmail.com>
2025-09-23 23:37:47 +0800

    refactor(Neovim): set different option for "vim-matchup"
    I'm not sure what `matchup_matchparen_offscreen` helps.
    
    Ref.: https://github.com/andymass/vim-matchup?tab=readme-ov-file#installation

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -33,9 +33,11 @@ return {
 
   {
     'andymass/vim-matchup',
-    init = function()
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end
+    opts = {
+      treesitter = {
+        stopline = 500,
+      }
+    }
   },
 
   {
