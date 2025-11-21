fe2aa687d9bc6f1c07e298b2e95776a67c796ca1
Gilbert Wong <gilbertwyw@gmail.com>
2025-11-21 21:23:42 +0800

    chore(Neovim): disable latex to suppress warning from "render-markdown.nvim"

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-    opts = {},
+    opts = { latex = { enabled = false } },
     lazy = false,
     keys = { { "<leader>tm", "<cmd>RenderMarkdown toggle<CR>", desc = "Toggle markdown rendering" } },
   },
