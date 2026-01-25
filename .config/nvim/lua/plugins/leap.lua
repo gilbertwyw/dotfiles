9889258231d50f43bbce4bdac763b3f346bcf1a0
Gilbert Wong <gilbertwyw@gmail.com>
2026-01-26 00:11:01 +0800

    chore(neovim): update source for "leap.nvim"

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,5 +1,5 @@
 return {
-  'ggandor/leap.nvim',
+  url = "https://codeberg.org/andyg/leap.nvim",
   config = function()
     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
     vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
