623315bcf3d84a008cdc83d280aefb753190f388
Gilbert Wong <gilbertwyw@gmail.com>
2026-04-02 21:27:25 +0800

    feat(Neovim): add "which-key" plugin

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,4 +1,23 @@
 return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+
   -- comment
   { 'numToStr/Comment.nvim',     opts = {} },
 
