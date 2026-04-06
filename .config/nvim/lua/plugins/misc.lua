bcc3c4df48bd4046ba586d2959276eecca4ff153
Gilbert Wong <gilbertwyw@gmail.com>
2026-04-06 09:04:30 +0800

    chore(Neovim): upgrade nvim-surround

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -30,9 +30,8 @@ return {
 
   {
     "kylechui/nvim-surround",
-    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    opts = {},
   },
 
   {
