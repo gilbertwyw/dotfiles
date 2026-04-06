70a18541af809ba0a04a790654b5b6077901af9b
Gilbert Wong <gilbertwyw@gmail.com>
2026-04-06 09:23:06 +0800

    chore(Neovim): oil.nvim configuration

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,9 @@
 return {
   {
     'stevearc/oil.nvim',
+    opts = {},
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
     config = function()
       require("oil").setup()
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
