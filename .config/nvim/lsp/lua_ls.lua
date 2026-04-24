e7b62fa009ccd5cc34bcb36370b63e17111083c4
Gilbert Wong <gilbertwyw@gmail.com>
2026-04-25 00:28:13 +0800

    feat(Neovim): enable "LSP setttings type annotations" in lua_ls

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -27,19 +27,14 @@ return {
       workspace = {
         checkThirdParty = false,
         library = {
-          vim.env.VIMRUNTIME
+          vim.env.VIMRUNTIME,
+          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+          vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
           -- Depending on the usage, you might want to add additional paths
           -- here.
           -- '${3rd}/luv/library'
           -- '${3rd}/busted/library'
         }
-        -- Or pull in all of 'runtimepath'.
-        -- NOTE: this is a lot slower and will cause issues when working on
-        -- your own configuration.
-        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-        -- library = {
-        --   vim.api.nvim_get_runtime_file('', true),
-        -- }
       }
     })
   end,
