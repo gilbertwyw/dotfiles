abaabed72f640db04baaae9cb4d46ec47ee9ffbc
Gilbert Wong <gilbertwyw@gmail.com>
2026-04-25 12:54:17 +0800

    refactor(Neovim): update leap.nvim configuration

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -4,23 +4,25 @@ return {
     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
     vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
 
-    vim.keymap.set({ 'x', 'o' }, 'R', function()
+    vim.keymap.set({ 'x', 'o' }, 'an', function()
       require('leap.treesitter').select {
-        -- To increase/decrease the selection in a clever-f-like manner,
-        -- with the trigger key itself (vRRRRrr...). The default keys
-        -- (<enter>/<backspace>) also work, so feel free to skip this.
-        opts = require('leap.user').with_traversal_keys('R', 'r')
+        opts = require('leap.user').with_traversal_keys('n', 'N')
+      }
+    end)
+
+    vim.keymap.set({ 'n', 'o' }, 'gs', function()
+      require('leap.remote').action {
+        -- Automatically enter Visual mode when coming from Normal.
+        input = vim.fn.mode(true):match('o') and '' or 'v'
       }
     end)
 
     -- Highly recommended: define a preview filter to reduce visual noise
-    -- and the blinking effect after the first keypress
-    -- (`:h leap.opts.preview`). You can still target any visible
-    -- positions if needed, but you can define what is considered an
-    -- exceptional case.
-    -- Exclude whitespace and the middle of alphabetic words from preview:
-    --   foobar[baaz] = quux
-    --   ^----^^^--^^-^-^--^
+    -- and the blinking effect after the first keypress.
+    -- For example, define word boundaries as the common case, that is, skip
+    -- preview for matches starting with whitespace or an alphabetic
+    -- mid-word character: foobar[baaz] = quux
+    --                     *    ***  ** * *  *
     require('leap').opts.preview = function(ch0, ch1, ch2)
       return not (
         ch1:match('%s')
@@ -28,14 +30,33 @@ return {
       )
     end
 
-    -- Define equivalence classes for brackets and quotes, in addition to
-    -- the default whitespace group:
-    require('leap').opts.equivalence_classes = {
-      ' \t\r\n', '([{', ')]}', '\'"`'
-    }
+    -- Enable the traversal keys to repeat the previous search without
+    -- explicitly invoking Leap (`<cr><cr>...` instead of `s<cr><cr>...`):
+    do
+      local clever = require('leap.user').with_traversal_keys
+      -- For relative directions, set the `backward` flags according to:
+      -- local prev_backward = require('leap').state['repeat'].backward
+      vim.keymap.set({ 'n', 'x', 'o' }, '<cr>', function()
+        require('leap').leap {
+          ['repeat'] = true, opts = clever('<cr>', '<bs>'),
+        }
+      end)
+      vim.keymap.set({ 'n', 'x', 'o' }, '<bs>', function()
+        require('leap').leap {
+          ['repeat'] = true, opts = clever('<bs>', '<cr>'), backward = true,
+        }
+      end)
+    end
 
-    -- Use the traversal keys to repeat the previous motion without
-    -- explicitly invoking Leap:
-    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+    -- Set automatic paste after remote yank operations:
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'RemoteOperationDone',
+      group = vim.api.nvim_create_augroup('LeapRemote', {}),
+      callback = function(event)
+        if vim.v.operator == 'y' and event.data.register == '"' then
+          vim.cmd('normal! p')
+        end
+      end,
+    })
   end
 }
